979cb3d4104131a138bfa1dae1cb6829028c9f48
richard lee <rockiee281@gmail.com>
2014-05-14 15:08:32 +0800

    wildcard match

--- head job/my/leetcode/Wildcard_Matching.py
+++ head job/my/leetcode/Wildcard_Matching.py
@@ -3,54 +3,56 @@ class Solution:
     # @param p, a pattern string
     # @return a boolean
     def isMatch(self, s, p):
-        #For specail cases
-        if s is None or p is None:
-            return False
+        star = False
+        index_s = 0
+        index_p = 0
+        cursor_s = 0
+        cursor_p = 0
         len_s = len(s)
         len_p = len(p)
-        if len_s == 0 and len_p == 0:
-            return True
-        if len_p == 0:
-            return False
-        if len_s == 0:
-            for a in p:
-                if a != '*':
+        while cursor_s < len_s:
+            #special case for cursor_p move the end of p
+            if cursor_p == len_p:
+                if not star:
                     return False
-            return True
+                else:
+                    index_s += 1
+                    cursor_s = index_s
+                    cursor_p = index_p
+                    continue
 
-        s_i = 0
-        p_i = 0
-        match_all_flag = False
-        while p_i < len_p and s_i < len_s:
-            if p[p_i] == '*':
-                match_all_flag = True
-                p_i += 1
-                s_i += 1
-            elif p[p_i] == '?':
-                p_i += 1
-                s_i += 1
+            if p[cursor_p] == '*':
+                while p[cursor_p] == '*':
+                    cursor_p += 1
+                    if cursor_p == len_p:
+                        return True
+                index_s = cursor_s
+                index_p = cursor_p
+                star = True
+                continue
+            elif p[cursor_p] == '?':
+                cursor_s += 1
+                cursor_p += 1
+                continue
             else:
-                if s[s_i] != p[p_i]:
-                    if not match_all_flag:
+                if p[cursor_p] != s[cursor_s]:
+                    if not star:
                         return False
                     else:
-                        s_i += 1
+                        index_s += 1
+                        cursor_s = index_s
+                        cursor_p = index_p
+                        continue
                 else:
-                    p_i += 1
-                    s_i += 1
+                    cursor_s += 1
+                    cursor_p += 1
+                    continue
 
-        # we go through the pattern string, and s is not process over
-        if p_i == len_p and s_i < len_s:
-            if p[-1] == '*':
-                return True
-            else:
+        #check the rest char of p
+        for index in xrange(cursor_p, len_p):
+            if p[index] != '*':
                 return False
-        # we go through the s, check if the rest char of P is '*'
-        if s_i == len_s:
-            if p_i < len_p:
-                for char in xrange(p_i, len_p):
-                    if p[char] != '*':
-                        return False
+
         return True
 
 
@@ -61,3 +63,5 @@ if __name__ == '__main__':
     print s.isMatch("ab", "?*") == True
     print s.isMatch("b", "*?*?") == False
     print s.isMatch('c', '*?*') == True
+    print s.isMatch('aa', 'a') == False
+
